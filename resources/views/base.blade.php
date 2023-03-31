25d8cbcc4afa9fc572a3bcba78adf4638dee0fe1
kristoperyo <129154871+kristoperyo@users.noreply.github.com>
2023-03-31 13:42:19 +0800

    Logo katabi ng  DICT

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -20,7 +20,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="{{ asset('') }}assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="{{ asset('') }}assets/img/favicon/DICT-Logo-Final-2-300x153 (1).ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
