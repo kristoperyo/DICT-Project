6c0f1b35b44d918cff9ba39289751e44208b635a
kristoperyo <129154871+kristoperyo@users.noreply.github.com>
2023-03-31 14:07:01 +0800

    removed dashboard

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -128,13 +128,6 @@
           <div class="menu-inner-shadow"></div>
 
           <ul class="menu-inner py-1">
-            <!-- Dashboard -->
-            <li class="menu-item active">
-              <a href="index.html" class="menu-link">
-                <i class="menu-icon tf-icons bx bx-home-circle"></i>
-                <div data-i18n="Analytics">Dashboard</div>
-              </a>
-            </li>
 
             <!-- Layouts -->
             <li class="menu-item">
